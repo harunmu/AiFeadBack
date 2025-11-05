73fecde39ad5decd096f8631ff621120fb719c7e
unknown <yuki.okadap@gmail.com>
2025-11-05 11:37:06 +0900

    ログインフォームと認証機能の作成

--- ai-feadback/app/page.tsx
+++ ai-feadback/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Start from "./components/Start";
 import Calendar from "./components/Calendar";
+import Login from "./components/Login";
+
 
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-       < Calendar />
+    <main className="flex min-h-screen items-center justify-center bg-white text-black font-sans">
+       <Login />
        <Start />
     </main>
   );
