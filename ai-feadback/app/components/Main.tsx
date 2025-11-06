2701f9cc8a362425f636cdfe5f0912846871e219
harumu <rushia0901@gmail.com>
2025-11-06 17:46:56 +0900

    ログ画面からチャット画面に履歴を持って移動

---
+++ ai-feadback/app/components/Main.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import React, { useState } from 'react';
+import Image from "next/image";
+import LogPreview from "./LogPreview";
+import Chat from "./Chat";
+import MenuBar from './MenuBar';
+
+type CurrentView = 'log' | 'chat';
+
+const Main = () => {
+  const [currentView, setCurrentView] = useState<CurrentView>('chat');
+  const [initialChatLog, setInitialChatLog] = useState<string[]>([]);
+
+  // ログをクリックした時の処理
+  const handleLogClick = (chatlog: string[]) => {
+    setInitialChatLog(chatlog);
+    setCurrentView('chat');
+  };
+
+  // 2. 現在のビューに基づいて表示するコンポーネントを決定
+  const renderView = () => {
+    switch (currentView) {
+      case 'log':
+        return <LogPreview onLogClick={handleLogClick} />;
+      case 'chat':
+        return <Chat initialChatLog={initialChatLog} />;
+      default:
+        return <Chat initialChatLog={initialChatLog} />;
+    }
+   };
+
+  return (
+    <main className="flex flex-col min-h-screen relative">
+      
+      <div className="flex-grow p-4 mb-16">
+        { renderView() }
+      </div>
+
+      <MenuBar 
+        currentView={currentView} 
+        onViewChange={setCurrentView} 
+      />
+      
+    </main>
+
+  );
+}
+
+export default Main
